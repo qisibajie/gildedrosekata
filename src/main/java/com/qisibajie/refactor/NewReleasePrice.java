8b14e360953891e9c6c556943d157952f8874d72
qisibajie <qisibajie@163.com>
2017-11-05 20:27:27 +0800

    [refactor-1: refactor second step: use stage pattern]

--- src/main/java/com/qisibajie/refactor/NewReleasePrice.java
+++ src/main/java/com/qisibajie/refactor/NewReleasePrice.java
@@ -9,4 +9,9 @@ public class NewReleasePrice extends Price {
     double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    int getFrequentRenterPoints(int daysRented) {
+        return daysRented > 1 ? 2 : 1;
+
+    }
 }
--- src/main/java/com/qisibajie/refactor/Price.java
+++ src/main/java/com/qisibajie/refactor/Price.java
@@ -5,4 +5,8 @@ abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
