c730a2464bd7c26889e864e213aeabf29edf4e98
qisibajie <qisibajie@163.com>
2017-11-19 20:20:05 +0800

    [refactor1: add test]

--- src/main/java/com/refactor/homework/GildedRose.java
+++ src/main/java/com/refactor/homework/GildedRose.java
@@ -3,6 +3,9 @@ package com.refactor.homework;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class GildedRose {
 
@@ -24,9 +27,99 @@ public class GildedRose {
         items.add(new Item("Conjured Mana Cake", 3, 6));
 
         updateQuality();
+
+
+        for(Item item : items){
+            if(item.getName().equals("+5 Dexterity Vest")) {
+                assertThat(item.getSellIn(), is(9));
+                assertThat(item.getQuality(), is(19));
+            }
+            if(item.getName().equals("Aged Brie")){
+                assertThat(item.getSellIn(), is(1));
+                assertThat(item.getQuality(), is(1));
+            }
+            if(item.getName().equals("Elixir of the Mongoose")){
+                assertThat(item.getSellIn(), is(4));
+                assertThat(item.getQuality(), is(6));
+            }
+            if(item.getName().equals("Sulfuras, Hand of Ragnaros")){
+                assertThat(item.getSellIn(), is(0));
+                assertThat(item.getQuality(), is(80));
+            }
+            if(item.getName().equals("Backstage passes to a TAFKAL80ETC concert")){
+                assertThat(item.getSellIn(), is(14));
+                assertThat(item.getQuality(), is(21));
+            }
+            if(item.getName().equals("Conjured Mana Cake")){
+                assertThat(item.getSellIn(), is(2));
+                assertThat(item.getQuality(), is(5));
+            }
+        }
+
+        for(int i=0; i<10; i++){
+            updateQuality();
+        }
+
+        for(Item item : items){
+            if(item.getName().equals("+5 Dexterity Vest")) {
+                assertThat(item.getSellIn(), is(-1));
+                assertThat(item.getQuality(), is(8));
+            }
+            if(item.getName().equals("Aged Brie")){
+                assertThat(item.getSellIn(), is(-9));
+                assertThat(item.getQuality(), is(20));
+            }
+            if(item.getName().equals("Elixir of the Mongoose")){
+                assertThat(item.getSellIn(), is(-6));
+                assertThat(item.getQuality(), is(0));
+            }
+            if(item.getName().equals("Sulfuras, Hand of Ragnaros")){
+                assertThat(item.getSellIn(), is(0));
+                assertThat(item.getQuality(), is(80));
+            }
+            if(item.getName().equals("Backstage passes to a TAFKAL80ETC concert")){
+                assertThat(item.getSellIn(), is(4));
+                assertThat(item.getQuality(), is(38));
+            }
+            if(item.getName().equals("Conjured Mana Cake")){
+                assertThat(item.getSellIn(), is(-8));
+                assertThat(item.getQuality(), is(0));
+            }
+
+        }
+
+        for(int i=0; i< 40; i++){
+            updateQuality();
+
+        }
+
+        for (Item item:items) {
+            if(item.getName().equals("+5 Dexterity Vest")) {
+                assertThat(item.getSellIn(), is(-41));
+                assertThat(item.getQuality(), is(0));
+            }
+            if(item.getName().equals("Aged Brie")){
+                assertThat(item.getSellIn(), is(-49));
+                assertThat(item.getQuality(), is(50));
+            }
+            if(item.getName().equals("Elixir of the Mongoose")){
+                assertThat(item.getSellIn(), is(-46));
+                assertThat(item.getQuality(), is(0));
+            }
+            if(item.getName().equals("Sulfuras, Hand of Ragnaros")){
+                assertThat(item.getSellIn(), is(0));
+                assertThat(item.getQuality(), is(80));
+            }
+            if(item.getName().equals("Backstage passes to a TAFKAL80ETC concert")){
+                assertThat(item.getSellIn(), is(-36));
+                assertThat(item.getQuality(), is(0));
+            }
+            if(item.getName().equals("Conjured Mana Cake")){
+                assertThat(item.getSellIn(), is(-48));
+                assertThat(item.getQuality(), is(0));
+            }
+        }
 }
-
-
 	
     public static void updateQuality()
     {
