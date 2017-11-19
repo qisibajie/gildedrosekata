40775ae667e463a865f9fa4110f9983afb7bf67b
qisibajie <qisibajie@163.com>
2017-11-19 22:38:49 +0800

    [refactor5: remove 50 if statement]

--- src/main/java/com/refactor/homework/Item.java
+++ src/main/java/com/refactor/homework/Item.java
@@ -2,16 +2,16 @@ package com.refactor.homework;
 
 public class Item {
     public String name;
-	public int sellIn; 
-    public int quality; 
-    
-    public Item(String name, int sellIn, int quality) {
-		this.setName(name);
-		this.setSellIn(sellIn);
-		this.setQuality(quality);
-	}
+    public int sellIn;
+    public int quality;
 
-	public void updateItem() {
+    public Item(String name, int sellIn, int quality) {
+        this.setName(name);
+        this.setSellIn(sellIn);
+        this.setQuality(quality);
+    }
+
+    public void updateItem() {
         if ((!"Aged Brie".equals(getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(getName())) {
             if (getQuality() > 0) {
                 if (!"Sulfuras, Hand of Ragnaros".equals(getName())) {
@@ -19,21 +19,15 @@ public class Item {
                 }
             }
         } else {
-            if (getQuality() < 50) {
-                setQuality(getQuality() + 1);
+            setQuality(notGreaterThanFifty(getQuality() + 1));
 
-                if ("Backstage passes to a TAFKAL80ETC concert".equals(getName())) {
-                    if (getSellIn() < 11) {
-                        if (getQuality() < 50) {
-                            setQuality(getQuality() + 1);
-                        }
-                    }
+            if ("Backstage passes to a TAFKAL80ETC concert".equals(getName())) {
+                if (getSellIn() < 11) {
+                    setQuality(notGreaterThanFifty(getQuality() + 1));
+                }
 
-                    if (getSellIn() < 6) {
-                        if (getQuality() < 50) {
-                            setQuality(getQuality() + 1);
-                        }
-                    }
+                if (getSellIn() < 6) {
+                    setQuality(notGreaterThanFifty(getQuality() + 1));
                 }
             }
         }
@@ -51,33 +45,40 @@ public class Item {
                         }
                     }
                 } else {
-                    setQuality(getQuality() - getQuality());
+                    setQuality(0);
                 }
             } else {
-                if (getQuality() < 50) {
-                    setQuality(getQuality() + 1);
-                }
+                setQuality(notGreaterThanFifty(getQuality() + 1));
             }
         }
     }
 
-	/* Generated getter and setter code */
+    private int notGreaterThanFifty(int quality) {
+        return quality > 50 ? 50 : quality;
+    }
+
+    /* Generated getter and setter code */
     public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSellIn() {
-		return sellIn;
-	}
-	public void setSellIn(int sellIn) {
-		this.sellIn = sellIn;
-	}
-	public int getQuality() {
-		return quality;
-	}
-	public void setQuality(int quality) {
-		this.quality = quality;
-	}
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
 }
