a0967030591c3dde31d77aa5bf0a850c375d9a53
qisibajie <qisibajie@163.com>
2017-11-19 22:14:04 +0800

    [refactor3: extract method and move method]

--- src/main/java/com/refactor/homework/Item.java
+++ src/main/java/com/refactor/homework/Item.java
@@ -10,7 +10,57 @@ public class Item {
 		this.setSellIn(sellIn);
 		this.setQuality(quality);
 	}
-    
+
+	public void updateItem(Item item) {
+        if ((!"Aged Brie".equals(item.getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+            if (item.getQuality() > 0) {
+                if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+                    item.setQuality(item.getQuality() - 1);
+                }
+            }
+        } else {
+            if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
+
+                if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+                    if (item.getSellIn() < 11) {
+                        if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
+                        }
+                    }
+
+                    if (item.getSellIn() < 6) {
+                        if (item.getQuality() < 50) {
+                            item.setQuality(item.getQuality() + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+            item.setSellIn(item.getSellIn() - 1);
+        }
+
+        if (item.getSellIn() < 0) {
+            if (!"Aged Brie".equals(item.getName())) {
+                if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName())) {
+                    if (item.getQuality() > 0) {
+                        if (!"Sulfuras, Hand of Ragnaros".equals(item.getName())) {
+                            item.setQuality(item.getQuality() - 1);
+                        }
+                    }
+                } else {
+                    item.setQuality(item.getQuality() - item.getQuality());
+                }
+            } else {
+                if (item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
+                }
+            }
+        }
+    }
+
 	/* Generated getter and setter code */
     public String getName() {
 		return name;
