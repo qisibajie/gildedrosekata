9cf9ee635369b50328daa06626e1104cae412141
qisibajie <qisibajie@163.com>
2017-12-18 11:53:29 +0800

    extract writePriceTo method

--- src/main/java/com/refactor/composite/XmlBuilder.java
+++ src/main/java/com/refactor/composite/XmlBuilder.java
@@ -47,15 +47,19 @@ public class XmlBuilder {
                 xml.append("'");
             }
             xml.append(">");
-            xml.append("<price");
-            xml.append(" currency='");
-            xml.append(getCurrentFor(product));
-            xml.append("'>");
-            xml.append(product.getPrice());
-            xml.append("</price>");
+            writePriceTo(xml, product);
             xml.append(product.getName());
             xml.append("</product>");
         }
+    }
+
+    private void writePriceTo(StringBuilder xml, Product product) {
+        xml.append("<price");
+        xml.append(" currency='");
+        xml.append(getCurrentFor(product));
+        xml.append("'>");
+        xml.append(product.getPrice());
+        xml.append("</price>");
     }
 
     private String getCurrentFor(Product product) {
