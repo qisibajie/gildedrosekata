2162aed28009f9f3b805c4eea0ec036c5b28b52a
qisibajie <qisibajie@163.com>
2017-12-18 22:51:46 +0800

    remove null judge

--- src/main/java/com/refactor/nullobject/introduce/SiteClient.java
+++ src/main/java/com/refactor/nullobject/introduce/SiteClient.java
@@ -2,25 +2,21 @@ package com.refactor.nullobject.introduce;
 
 public class SiteClient {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Site site = new Site();
         Customer customer = site.getCustomer();
         BillingPlan plan;
-        if(customer.isNull()){
-           plan = BillingPlan.basic();
-        }else {
+        if (customer.isNull()) {
+            plan = BillingPlan.basic();
+        } else {
             plan = customer.getPlan();
         }
         String customerName;
-        if(customer.isNull()){
-            customerName ="occupant";
-        }else {
-            customerName = customer.getName();
-        }
+        customerName = customer.getName();
         int weeksDelinquent;
-        if(customer.isNull()){
+        if (customer.isNull()) {
             weeksDelinquent = 0;
-        }else{
+        } else {
             weeksDelinquent = customer.getHistory().getWeeksDelinquentInLastYear();
         }
     }
